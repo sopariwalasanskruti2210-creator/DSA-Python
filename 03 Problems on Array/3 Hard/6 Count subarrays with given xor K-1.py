757c27576f4e85da7c149e08051cca2a2e094371
sopariwalasanskruti2210-creator <sopariwalasanskruti2210@gmail.com>
2026-08-12 17:11:04 +0530

    Commit

---
+++ 03 Problems on Array/3 Hard/6 Count subarrays with given xor K-1.py
@@ -0,0 +1,21 @@
+'''
+Given an array of integers nums and an integer k, 
+return the total number of subarrays whose XOR equals to k.
+'''
+nums = [4, 2, 2, 6, 4]
+k = 6
+n = len(nums)
+count = 0
+XOR = 0
+for i in range(n):
+    XOR = i
+    for j in range(i+1,n):
+        if nums[j]==k:
+            count+=1
+        else:
+            XOR^=nums[j]
+            if XOR==k:
+                count+=1
+
+print(count)
+
---
+++ 03 Problems on Array/3 Hard/6 Count subarrays with given xor k-2.py
@@ -0,0 +1,26 @@
+'''
+Given an array of integers nums and an integer k, 
+return the total number of subarrays whose XOR equals to k.
+'''
+nums = [4, 2, 2, 6, 4]
+k = 6
+n = len(nums)
+current_XOR = 0
+count = 0
+freq = {}
+for i in range(n):
+
+    current_XOR^=nums[i]
+    if current_XOR==k:
+        count+=1
+
+    target = current_XOR^k
+    if target in freq:
+        count+=freq[target]
+
+    if current_XOR not in freq:
+        freq[current_XOR]=1
+    else:
+        freq[current_XOR]+=1
+
+print(count)
